5cd5e26682303cc1bd4e623e47593a94b8601ed9
victor <vrubinr501@gmail.com>
2025-07-11 16:53:37 +0200

    Initial commit

---
+++ src/main/java/com/victor/proyectoFXBackEnd/ProyectoFxBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.victor.proyectoFXBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFxBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFxBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victor/proyectoFXBackEnd/ProyectoFxBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.proyectoFXBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFxBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
